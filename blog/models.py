7cf1986732d0eaa2d5dce41bebc59e7b8b9b4927
myomyatkhinkhin <myomyatkhin11sg@gmail.com>
2026-09-16 13:10:06 +0900

    Update project

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     def save (self, *args, **kwargs):
        if not self.slug:
           self.slug = slugify(self.title)
-    super().save(*args, **kwargs)
+       super().save(*args, **kwargs)
 
     def __str__(self):
      return self.title
--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'blog',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = ["127.0.0.1"]
 
 ROOT_URLCONF = 'config.urls'
 
