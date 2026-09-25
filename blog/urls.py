d530d7f7edc9f5e6d6931c343332680bf4a8c9ce
myomyatkhinkhin <myomyatkhin11sg@gmail.com>
2026-09-25 15:23:12 +0900

    Refactor list/detail views to CBVs; add category sidebar

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
 from.import views
+from.views import PostListView,PostDetailView, about, contact
 
 urlpatterns = [
-    path('', views.post_list, name='home'),
+    path('', views.PostListView.as_view(), name='home'),
     path('posts/<slug:slug>/',views.post_detail, name='post_detail'),
     path('about/', views.about, name='about'),
     path('contact/',views.contact, name='contact'), 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.views.generic import ListView,DetailView
+from .models import Post, Category
 
 def post_list(request,):
     posts = Post.objects.filter(status="published").order_by("-created_at")
@@ -15,7 +16,31 @@ def post_detail(request, slug):
     return render(request, "blog/post_detail.html", {"post": post})
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html",{"team": "DjangoBlog Team"})
 
 def contact(request):
     return render(request, "blog/contact.html")
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    paginate_by = 6
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            status="published"
+        ).order_by("-created_at")
+
+    def get_context_data(self, **kwargs):
+     context = super().get_context_data(**kwargs)
+     context["categories"] = Category.objects.all()
+     return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+    def get_queryset(self):
+        return Post.objects.filter(status="published")
